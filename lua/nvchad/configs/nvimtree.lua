d1828110ab627823e4a22dcf6839a887668ed22b
EloyTolosaDev <eloytolosawork@gmail.com>
2025-02-16 13:09:54 +0100

    Commenting the lines to see what they do

--- lua/nvchad/configs/nvimtree.lua
+++ lua/nvchad/configs/nvimtree.lua
@@ -4,12 +4,12 @@ return {
   filters = { dotfiles = false },
   disable_netrw = true,
   hijack_cursor = true,
-  sync_root_with_cwd = true,
+  -- sync_root_with_cwd = true,
   update_focused_file = {
     enable = true,
     update_root = true,  -- Ensure this is set to true to update root
   },
-  update_cwd = true,  -- Ensure this is enabled to sync cwd with the file tree
+  -- update_cwd = true,  -- Ensure this is enabled to sync cwd with the file tree
   view = {
     width = 30,
     preserve_window_proportions = true,
