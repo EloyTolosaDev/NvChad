6e6f8c5df98d1f83419f9099c31172679ff1da44
Eloy Tolosa <etolosa@proadata.com>
2025-02-17 11:37:53 +0100

    Added nvimtree default config

--- lua/nvchad/configs/nvimtree.lua
+++ lua/nvchad/configs/nvimtree.lua
@@ -5,3 +5,38 @@ vim.cmd([[
     autocmd VimEnter * if argc() == 0 || isdirectory(argv(0)) | execute 'cd ' . argv(0) | NvimTreeToggle | endif
   augroup END
 ]])
+
+dofile(vim.g.base46_cache .. "nvimtree")
+return {
+  filters = { dotfiles = false },
+  disable_netrw = true,
+  hijack_cursor = true,
+  -- sync_root_with_cwd = true,
+  update_focused_file = {
+    enable = true,
+    update_root = true,  -- Ensure this is set to true to update root
+  },
+  -- update_cwd = true,  -- Ensure this is enabled to sync cwd with the file tree
+  view = {
+    width = 30,
+    preserve_window_proportions = true,
+  },
+  renderer = {
+    root_folder_label = false,
+    highlight_git = true,
+    indent_markers = { enable = true },
+    icons = {
+      glyphs = {
+        default = "󰈚",
+        folder = {
+          default = "",
+          empty = "",
+          empty_open = "",
+          open = "",
+          symlink = "",
+        },
+        git = { unmerged = "" },
+      },
+    },
+  },
+}
