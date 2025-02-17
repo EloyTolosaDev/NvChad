5d68a56ebe0c7d5348ee19eecc14fb6ed1a6aab0
Eloy Tolosa <etolosa@proadata.com>
2025-02-17 11:33:19 +0100

    Testing what does work or not in my working laptop

--- lua/nvchad/configs/nvimtree.lua
+++ lua/nvchad/configs/nvimtree.lua
@@ -1,1 +1,7 @@
-
+-- Automatically open nvim-tree for the directory passed in `nvim .`
+vim.cmd([[
+  augroup NvimTreeAutoOpen
+    autocmd!
+    autocmd VimEnter * if argc() == 0 || isdirectory(argv(0)) | execute 'cd ' . argv(0) | NvimTreeToggle | endif
+  augroup END
+]])
